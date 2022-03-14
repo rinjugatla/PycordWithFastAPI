b2b2261d132535ac02304c6aa373d43ce8264112
rinjugatla <rin.jugatla@gmail.com>
2022-03-14 12:58:01 +0900

    参加人数が多い(2万人など)サーバの場合にBOTが不定期にオフラインになる不具合を修正

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import discord
 from discord.ext import commands
 from fastapi import FastAPI
 import uvicorn
@@ -10,7 +11,10 @@ from apis.v1 import ApiV1
 class DiscordBot():
     def __init__(self):
         self.app = FastAPI()
-        self.bot = commands.Bot(heartbeat_timeout=20)
+        self.bot = commands.Bot(
+            heartbeat_timeout=20,
+            intents=self.create_intents(),
+            chunk_guilds_at_startup=False)
 
         api_v1 = ApiV1(self.bot)
         self.app.include_router(api_v1.router)
@@ -22,6 +26,21 @@ class DiscordBot():
             for name in const.COG_NAMES:
                 self.bot.load_extension(name)
             asyncio.create_task(self.bot.start(const.BOT_TOKEN))
+
+    def create_intents(self) -> discord.Intents:
+        """インテンツ作成
+
+        https://github.com/Pycord-Development/pycord/issues/872
+        サーバへの参加人数が非常に多く、guilds, membersが同時に有効な場合エラー
+        chunk_guilds_at_startup=Falseで対策
+
+        Returns:
+            discord.Intents: _description_
+        """
+        intents = discord.Intents.all()
+        intents.typing = False
+        intents.presences = False
+        return intents
 
     def start(self):
         uvicorn.run(self.app, host=const.HOST, port=const.PORT)
